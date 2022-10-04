97ff93ae71c6e6a6e2cb08b121499771abbddbfc
huynh02-stu <114965223+huynh02-stu@users.noreply.github.com>
2022-10-03 21:33:29 -0700

    Update CSdict.java
    Switch statement for command outline

--- a1-initial-code/CSdict.java
+++ a1-initial-code/CSdict.java
@@ -10,12 +10,15 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.io.BufferedReader;
 
+import java.io.*;
+import java.lang.Object.*;
+import java.net.*;
+
 //
 // This is an implementation of a simplified version of a command
 // line dictionary client. The only argument the program takes is
 // -d which turns on debugging output. 
 //
-
 
 public class CSdict {
     static final int MAX_LEN = 255;
@@ -28,6 +31,7 @@ public class CSdict {
     public static void main(String [] args) {
         byte cmdString[] = new byte[MAX_LEN];
 	int len;
+
 	// Verify command line arguments
 	
         if (args.length == PERMITTED_ARGUMENT_COUNT) {
@@ -43,6 +47,10 @@ public class CSdict {
             return;
         }
 
+	//establishes loop until user leaves	
+	int loopcondition = 0;
+
+	while (loopcondition == 0) {
 
 	// Example code to read command line input and extract arguments.
 	
@@ -59,21 +67,68 @@ public class CSdict {
 	    command = inputs[0].toLowerCase().trim();
 	    // Remainder of the inputs is the arguments. 
 	    arguments = Arrays.copyOfRange(inputs, 1, inputs.length);
-
+		
+		/*
 	    System.out.println("The command is: " + command);
 	    len = arguments.length;
 	    System.out.println("The arguments are: ");
 	    for (int i = 0; i < len; i++) {
 		System.out.println("    " + arguments[i]);
 	    }
-	    System.out.println("Done.");
-	    
+	    System.out.println("Done."); 
+		*/
+
+
+		////////////
+
+		switch (command) {
+			case "open": 
+				String hostName = arguments[0];
+				int portNumber =  Integer.parseInt(arguments[1]);
+
+				Socket echoSocket = new Socket(hostName, portNumber);  
+
+				PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+
+				BufferedReader in =new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+				BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+				System.out.println("success with socket implementation");
+				break;
+
+			case "dict":
+				System.out.println("command = dict");
+				break;
+			case "set":
+				System.out.println("command = set");
+				break;
+			case "define":
+				System.out.println("command = define");
+				break;
+			case "match":
+				System.out.println("command = match");
+				break;
+			case "prefixmatch":
+				System.out.println("command = prefixmatch");
+				break;
+			case "close":
+				System.out.println("command = close");
+				break;
+			case "quit":
+				System.out.println("command = quit");
+				System.exit(-1);
+			default :
+				System.out.println("900 Invalid command");
+				break;
+		}
+
+		///////////
 	    
 	} catch (IOException exception) {
 	System.err.println("998 Input error while reading commands, terminating.");
             System.exit(-1);
 	}
+
     }
+	}
 }
-    
-    
